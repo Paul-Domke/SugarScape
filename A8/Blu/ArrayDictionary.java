e40d79f47ac17a4644ba5e218148644e5a58321a
Paul-Domke <paul.domke17@ncf.edu>
2018-04-13 13:41:42 -0400

    A8

--- A8/Blu/ArrayDictionary.java
+++ A8/Blu/ArrayDictionary.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-
+import java.util.Collections;
 public class ArrayDictionary<K extends Comparable<K>, V> implements Dictionary<K,V>{
     ArrayList<Pair<K, V>> dict;
     
-    // Must run at constant time (O(1) operations)
     public ArrayDictionary(){
         dict = new ArrayList<Pair<K, V>>();
     }
@@ -16,7 +15,6 @@ public class ArrayDictionary<K extends Comparable<K>, V> implements Dictionary<K
         return dict.size();
     }
     
-    // must run in O(size()) time
     public V remove(K key){
         for(int i = 0; i < dict.size(); i++){
             K currentKey = dict.get(i).getKey();
@@ -31,38 +29,59 @@ public class ArrayDictionary<K extends Comparable<K>, V> implements Dictionary<K
     }
     
     public V get(K key){
-        for(int i = 0; i < dict.size(); i++){
-            K currentKey = dict.get(i).getKey();
-            V currentValue = dict.get(i).getValue();
-            if(currentKey.compareTo(key) == 0){
-                return currentValue;
+        int start = 0;
+        int end = dict.size();
+        while(start != end){
+            int middle = (end - start)/2;
+            Pair<K, V> currentPair = dict.get(middle);
+            if(key.compareTo(currentPair.getKey()) == 0){
+                return currentPair.getValue();
+            }
+            if(key.compareTo(currentPair.getKey()) < 0){
+                start = middle + 1;
+            }
+            if(key.compareTo(currentPair.getKey()) > 0){
+                end = middle;
             }
         }
         return null;
     }
     
-    // methods must run in O(log2(size())) time.
     public boolean contains(K key){
-        for(int i = 0; i < dict.size(); i++){
-            K currentKey = dict.get(i).getKey();
-            if(currentKey.compareTo(key) == 0){
+        int start = 0;
+        int end = dict.size();
+        while(start != end){
+            int middle = start + end/2;
+            int comp = dict.get(middle).getKey().compareTo(key);
+            if(comp == 0){
                 return true;
+            }
+            if(comp < 0){
+                start = middle + 1;
+            }
+            if(comp > 0){
+                end = middle;
             }
         }
         return false;
     }
     
     public void put(K key, V value){
+        Pair<K,V> temp = new Pair<K,V>(key, value);
+        if(dict.size() == 0){
+            dict.add(temp);
+            return;
+        }
         for(int i = 0; i < dict.size(); i++){
             Pair<K, V> currentPair = dict.get(i);
             K currentKey = currentPair.getKey();
-            V currentValue = currentPair.getValue();
             if(currentKey.compareTo(key) == 0){
-                dict.set(i, currentPair);
+                dict.set(i, temp);
                 return;
             }
-            while(){
-                
+            if(key.compareTo(currentKey) > 0){
+                dict.add(temp);
+                return;
             }
         }
     }
@@ -74,6 +93,10 @@ public class ArrayDictionary<K extends Comparable<K>, V> implements Dictionary<K
         //and no keys that are already found in the dictionary. 
         //The total time must not exceed O(n*log n), 
         //where n is the number of keys in the dictionary after data is added.
-        //You may use Collections.sort() if necessary.  
+        //You may use Collections.sort() if necessary.
+        for(Pair currentPair : data){
+            dict.add(currentPair);
+        }
+        Collections.sort(dict);
     }
 }
--- A8/Blu/Replacer.java
+++ A8/Blu/Replacer.java
@@ -1,4 +1,49 @@
-public class Replacer
-{
-    
+import java.util.*;
+public class Replacer{ 
+   public static void main(String[] args){
+       Scanner s = new Scanner(System.in);
+       int count = 0;
+       String key = "";
+       String value = "";
+       ArrayDictionary<String, String> dict = new ArrayDictionary<String, String>();
+       ArrayList<String> words = new ArrayList<String>();
+       
+       while(s.hasNext()){
+            String n = s.next();
+            if(n.equals("done")){
+               break;
+            }
+            if(count == 1){
+               value = n;
+               dict.put(key, value);
+               count = -1;
+            }
+            if(count == 0){
+               key = n;
+            }
+            count++;
+       }
+        
+       while(s.hasNext()){ 
+           String n = s.next();
+           if(n.equals("DONE")){
+               break;
+            }
+           words.add(n);
+       }
+       
+       for(String w : words){
+           if(dict.contains(w) == true){
+               System.out.println("Im here!");
+               int i = words.indexOf(w);
+               words.set(i, dict.get(w));
+            }
+       }
+        
+       for(int i = 0; i < words.size(); i++){
+           System.out.print(words.get(i) + " ");
+       }
+       
+       System.out.println();
+   }
 }
